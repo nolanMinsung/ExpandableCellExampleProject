70ff21cd08a283e1bc769df17dc212843d98f4e8
Minsung Kim <mskim4048@naver.com>
2025-03-24 12:00:53 +0900

    create project "FoldableCellExample"

---
+++ FoldableCellExample/FoldableCellExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoldableCellExample
+//
+//  Created by 김민성 on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
