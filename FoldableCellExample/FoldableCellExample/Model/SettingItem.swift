ed8de08ae523707e9c7d9ee1f5fa8c484f9f55fa
Minsung Kim <mskim4048@naver.com>
2025-03-24 12:27:56 +0900

    Add DummyData

---
+++ FoldableCellExample/FoldableCellExample/Model/SettingItem.swift
@@ -0,0 +1,16 @@
+//
+//  SettingItem.swift
+//  FoldableCellExample
+//
+//  Created by 김민성 on 3/24/25.
+//
+
+import UIKit
+
+struct SettingItem {
+    var title: String
+    var icon: UIImage?
+    var description1: String
+    var description2: String
+}
+
